537bbbd5e67edf4ecda6523705cc5b17096e497f
Gaurav Patel <Gaurav.Patel@asmadiya.com>
2025-11-24 11:43:06 +0530

    Vite config json changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';  // This is the import line
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
@@ -7,6 +7,7 @@ export default defineConfig({
         headers: {
             'Cross-Origin-Embedder-Policy': 'require-corp',
             'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Resource-Policy': 'cross-origin',
         },
     },
 });
